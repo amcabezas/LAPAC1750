dda17dbb3608a6c3c84aeafb76def0db52b46224
Alexis Cabezas Navarrete <alexis.cabezas.navarrete@gmail.com>
2017-08-18 12:51:35 -0300

    Add files via upload

---
+++ Cluster.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+import time
+from datetime import datetime
+
+report = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+
+driver = webdriver.Chrome('/usr/bin/chromedriver')
+driver.implicitly_wait(30)
+# IP de Linksys LAPAC1750 Cluster. 
+base_url = "http://192.168.69.181/"
+driver.get(base_url)
+#Busca las etiquetas e introduce usuario y contraseña.
+driver.find_element_by_name("login_name").clear()
+driver.find_element_by_name("login_name").send_keys("sistemas")
+driver.find_element_by_name("login_pwd").clear()
+driver.find_element_by_name("login_pwd").send_keys("iebsistemas")
+driver.find_element_by_css_selector("input[name=\"login\"]").click()
+driver.get(base_url+'Cluster_client.htm')
+#Vemos el código fuente de la página.
+web = driver.page_source.encode('utf-8')
+#Luego buscamos las etiquetas que poseen los campos de los clientes conectados y los recorremos para realizar la contarlos.
+html = BeautifulSoup(web,'lxml')
+tabla = html.find_all('tr', attrs={'class':'section-row'})
+i = 0
+for row in tabla:
+    #print row.getText()
+    i = i+1
+
+print i,report
+
+#def grabartxt():
+#    archi=open('/home/ieb/Recolector/datos-ag.txt','a')
+#    archi.write(str(i)+';'+str(report)+'\n')
+#    archi.close()
+
+#grabartxt()
+driver.quit()
+display.stop()
